36ffbc56a6dc2f41423ee3fd61007605a63d257d
Natalia Basalaeva <nuts9905@yahoo.com>
2022-03-10 01:58:13 +0100

    Final result

--- config.py
+++ config.py
@@ -1,9 +1,14 @@
 from flask import Flask
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
 # ... add more variables here as needed
 
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    #SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
